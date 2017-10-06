c319ec93e8e7340b2d7dab880bf3f28821b4899c
Krisztian Szucs <krisztians0818@gmail.com>
2017-10-06 12:55:16 +0200

    feat(unique chars): test file created, one test runs

---
+++ uniquechars/test_unique_chars.py
@@ -0,0 +1,19 @@
+import unittest
+import unique_chars
+
+class Unique_chars_test(unittest.TestCase):
+    
+    def unique_characters(self):
+       
+        self.assertEqual(unique_chars.unique_characters(""), "")
+
+    def unique_characters(self):
+        pass
+        
+    
+    def unique_characters(self):
+        pass
+        
+
+if __name__ == '__main__':
+    unittest.main()
